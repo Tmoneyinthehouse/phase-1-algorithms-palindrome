253c05d06bc95208ee0d5fc864704b30727aad15
Tyler Brown <tylerbrown487@gmail.com>
2023-05-28 18:54:22 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let reverseWord = word.split('').reverse().join('');
+  return reverseWord === word;
 }
 
 /* 
-  Add your pseudocode here
+  call on functrion isPalindrome
+  find if the argument is a palindrome or not
+  return true/false depending on end value (if it's a palindrome or not)
 */
 
 /*
-  Add written explanation of your solution here
+  The argument is taken in and used to make a new variable that is turned into an array of each character in the word, reversed in order, and finally joins them back together into a new string.
 */
 
 // You can run `node index.js` to view these console logs
